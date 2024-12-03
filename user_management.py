d696e6a3fd567629ca2968e7fddb85143df1f550
Szymon Grabarczyk <xcezor@gmail.com>
2024-12-03 09:29:25 +0100

    save content

---
+++ user_management.py
@@ -0,0 +1,28 @@
+import json
+import random
+import re
+import os
+
+new_user = []
+
+def add_user(user_data):
+    '''
+        "user_id": 1,
+        "name": "Szymon G",
+        "nip": 9999,
+        "pesel": "12345678912",
+        "regon": 1111,
+        "password": "abcdef123"
+        "status": "Active"
+    '''
+    new_user["user_id"] = 1
+    return 0
+#def remove_user(user_id)
+#def edit_user(user_id, updated_data)
+#def validate_nip(nip)
+#def validate_pesel(pesel)
+#def validate_regon(regon)
+#def generate_password()
+#def validate_password(password)
+
+print("Welcome to the user registration!")
