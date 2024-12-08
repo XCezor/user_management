72789e0649f7176a0680b6a9f3e552a5c0a6f92f
Szymon G <xcezor@gmail.com>
2024-12-08 16:39:49 +0100

    edit_user preparation

--- user_management.py
+++ user_management.py
@@ -66,8 +66,8 @@ def validate_password(password):
 print("Welcome to the user registration!")
 app_running = True
 while app_running:
-    option = int(input(f"What would you like to do?\n1. Register\n"))
-    if option == 1:
+    option = input(f"What would you like to do?\n1. Register\n2. Edit user\n")
+    if option == "1":
         user_data_list = {}
         user_data_list["id"] = 1
         user_data_list["username"] = input("Enter your full name: ")
@@ -78,3 +78,51 @@ while app_running:
         user_data_list["status"] = "Active"
 
         add_user(user_data_list)
+    elif option == "2":
+        data = load_data()
+        username_correct = False
+        while username_correct == False:
+            user_count = 0
+            login_username = input("Enter your username (Full name): ")
+            for value in data:
+                if value["username"] == login_username:
+                    username_correct = True
+                    user_count += 1
+                    break
+                else:
+                    continue
+            if user_count == 0:
+                print("Incorrect username, try again.\n")
+        password_correct = False
+        while password_correct == False:
+            password_count = 0
+            login_password = input("Enter your password: ")
+            for value in data:
+                if value["username"] == login_username and value["password"] == login_password:
+                    print(value["username"])
+                    password_correct = True
+                    password_count += 1
+
+                    print("Login successful!")
+                    edit_data = input("What you want to edit?\n1. Name\n2. NIP\n3. PESEL\n4. Regon\n5. Password\n")
+                    if edit_data == "1":
+                        new_username = input("Enter new name: ")
+                        value["username"] = new_username
+                    elif edit_data == "2":
+                        new_nip = input("Enter new NIP: ")
+                        value["nip"] = new_nip
+                    elif edit_data == "3":
+                        new_pesel = input("Enter new PESEL: ")
+                        value["pesel"] = new_pesel
+                    elif edit_data == "4":
+                        new_regon = input("Enter new Regon: ")
+                        value["regon"] = new_regon
+                    elif edit_data == "5":
+                        new_password = input("Enter new password: ")
+                        value["password"] = new_password
+                    edit_user(value["id"], value)
+                    break
+                else:
+                    continue
+            if password_count == 0:
+                print("Incorrect password, try again.\n")
